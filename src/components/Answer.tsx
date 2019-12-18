cc57c378dab142aeec6a223e8dba655e5b48b443
GuiLec <glecallier@yahoo.com>
2019-12-18 21:12:24 +0100

    add answer to answers area

---
+++ src/components/Answer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {styled} from '../lib/styled';
+
+const Touchable = styled.TouchableOpacity`
+  border-radius: ${props => props.theme.borderRadius.small};
+  background-color: ${props => props.theme.colors.lightGray};
+  padding-horizontal: ${props => props.theme.gridUnit * 2}px;
+  padding-vertical: ${props => props.theme.gridUnit}px;
+  ${props => props.theme.shadows.medium}
+  margin-horizontal: ${props => props.theme.gridUnit * 2}px;
+  margin-vertical: ${props => props.theme.gridUnit}px;
+`;
+
+const Text = styled.Text`
+  color: ${props => props.theme.colors.darkGray};
+  font-size: ${props => props.theme.fontSizes.medium};
+  font-weight: 700;
+`;
+
+export const Answer = () => (
+  <Touchable>
+    <Text>Hello</Text>
+  </Touchable>
+);
--- src/pages/Home/components/AnswersArea/AnswersArea.tsx
+++ src/pages/Home/components/AnswersArea/AnswersArea.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import {styled} from '../../../../lib/styled';
 import Collapsible from 'react-native-collapsible';
+import {Answer} from '../../../../components/Answer';
 
 const Container = styled.View`
-  height: 100;
+  padding-horizontal: ${props => props.theme.gridUnit * 6}px;
+  padding-bottom: ${props => props.theme.gridUnit * 3}px;
+  padding-top: ${props => props.theme.gridUnit}px;
+  background-color: ${props => props.theme.colors.white};
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
 `;
 
 interface Props {
@@ -13,7 +21,17 @@ interface Props {
 export const AnswersArea = (props: Props) => {
   return (
     <Collapsible collapsed={props.iscollapsed}>
-      <Container />
+      <Container>
+        <Answer />
+        <Answer />
+        <Answer />
+        <Answer />
+        <Answer />
+        <Answer />
+        <Answer />
+        <Answer />
+        <Answer />
+      </Container>
     </Collapsible>
   );
 };
