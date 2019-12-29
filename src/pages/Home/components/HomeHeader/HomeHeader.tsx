86b65f8a4f72c65579b406c67f0613de54f81072
GuiLec <glecallier@yahoo.com>
2019-12-29 14:47:38 +0100

    Add caret down on play button

--- src/pages/Home/components/HomeHeader/HomeHeader.tsx
+++ src/pages/Home/components/HomeHeader/HomeHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {styled} from '../../../../lib/styled';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import {TouchableOpacity} from 'react-native';
+import {TouchableOpacity, View} from 'react-native';
 import {computeUniformHitSlop} from '../../../../lib/utils';
 
 const Container = styled.View`
@@ -16,6 +16,13 @@ const HeaderIcon = styled(Icon)`
   color: ${props => props.theme.colors.lightGray};
 `;
 
+const CaretDownIcon = styled(Icon)`
+  color: ${props => props.theme.colors.lightGray};
+  position: absolute;
+  right: -5;
+  bottom: -5;
+`;
+
 const HeaderTitle = styled.Text`
   font-family: Pacifico;
   font-size: ${props => props.theme.fontSizes.big};
@@ -28,14 +35,19 @@ interface Props {
 
 export const HomeHeader = (props: Props) => (
   <Container>
-    <TouchableOpacity onPress={() => {}} hitSlop={computeUniformHitSlop(20)}>
-      <HeaderIcon name="bars" size={20} />
-    </TouchableOpacity>
+    <View>
+      <TouchableOpacity onPress={() => {}} hitSlop={computeUniformHitSlop(20)}>
+        <HeaderIcon name="bars" size={20} />
+      </TouchableOpacity>
+    </View>
     <HeaderTitle>DCM</HeaderTitle>
-    <TouchableOpacity
-      onPress={props.pickDictation}
-      hitSlop={computeUniformHitSlop(20)}>
-      <HeaderIcon name="play" size={20} />
-    </TouchableOpacity>
+    <View>
+      <TouchableOpacity
+        onPress={props.pickDictation}
+        hitSlop={computeUniformHitSlop(20)}>
+        <HeaderIcon name="play" size={20} />
+        <CaretDownIcon name="caret-down" size={10} />
+      </TouchableOpacity>
+    </View>
   </Container>
 );
