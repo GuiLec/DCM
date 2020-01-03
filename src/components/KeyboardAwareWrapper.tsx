1d63f7bd2e650bff2b639fbd5acbd2dbafbb1f90
GuiLec <glecallier@yahoo.com>
2020-01-03 20:28:31 +0100

    Add keyboard aware wrapper on text input are

---
+++ src/components/KeyboardAwareWrapper.tsx
@@ -0,0 +1,39 @@
+import React, {ReactNode} from 'react';
+import {KeyboardAvoidingView, NativeModules, Platform} from 'react-native';
+import {Header} from 'react-navigation-stack';
+
+import {styled} from '../lib/styled';
+
+interface Props {
+  children: ReactNode | ReactNode[];
+}
+
+const StyledKeyboardAvoidingView = styled(KeyboardAvoidingView)`
+  flex: 1;
+`;
+
+let statusBarHeight = 0;
+
+/**
+ * StatusBarManager.getHeight is not a function on android
+ */
+if (Platform.OS === 'ios') {
+  NativeModules.StatusBarManager.getHeight(({height}: {height: number}) => {
+    statusBarHeight = height;
+  });
+}
+
+/**
+ * Keyboard avoiding view is not aware of react navigation header and status bar.
+ * We have to put an offset equal to the header height and the status bar height
+ */
+export const KeyboardAwareWrapper = (props: Props) =>
+  Platform.OS === 'ios' ? (
+    <StyledKeyboardAvoidingView
+      behavior="padding"
+      keyboardVerticalOffset={Header.HEIGHT + statusBarHeight}>
+      {props.children}
+    </StyledKeyboardAvoidingView>
+  ) : (
+    <>{props.children}</>
+  );
--- src/pages/AuthorPage/components/TextInputArea/TextInputArea.tsx
+++ src/pages/AuthorPage/components/TextInputArea/TextInputArea.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {styled} from '../../../../lib/styled';
-import {ScrollView, TextInput} from 'react-native';
+import {ScrollView, TextInput, Keyboard} from 'react-native';
+import {KeyboardAwareWrapper} from '../../../../components/KeyboardAwareWrapper';
 
-const Container = styled.View`
+const Container = styled.TouchableOpacity`
   padding: ${props => props.theme.gridUnit * 2}px;
   margin: ${props => props.theme.gridUnit * 2}px;
   flex: 1;
@@ -10,18 +11,27 @@ const Container = styled.View`
   border-width: 1px;
 `;
 
+const Title = styled.Text`
+  font-size: ${props => props.theme.fontSizes.large};
+  font-weight: bold;
+`;
+
+const TextArea = styled(TextInput)`
+  min-height: ${props => props.theme.gridUnit * 20}px;
+  margin: ${props => props.theme.gridUnit * 4}px;
+  background-color: ${props => props.theme.colors.white};
+  flex: 1;
+`;
+
 interface Props {}
 
 export const TextInputArea = (props: Props) => {
   return (
-    <Container>
-      <ScrollView style={{flex: 1}}>
-        <TextInput
-          multiline={true}
-          numberOfLines={4}
-          placeholder="J'écris mon text ici..."
-        />
-      </ScrollView>
+    <Container onPress={Keyboard.dismiss} activeOpacity={1}>
+      <KeyboardAwareWrapper>
+        <Title>Le text de ma nouvelle tictée</Title>
+        <TextArea multiline={true} placeholder="J'écris mon text ici..." />
+      </KeyboardAwareWrapper>
     </Container>
   );
 };
