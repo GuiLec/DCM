d9885196c22b135482acaf2e91a49bbd9d7bc388
GuiLec <glecallier@yahoo.com>
2020-01-09 09:22:18 +0100

    Limits AnwerArea height

--- src/pages/Home/components/AnswersArea/AnswersArea.tsx
+++ src/pages/Home/components/AnswersArea/AnswersArea.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import {StyleSheet} from 'react-native';
 import {styled} from '../../../../lib/styled';
 import Collapsible from 'react-native-collapsible';
 import {Answer} from '../../../../components/Answer';
 import {Choice} from '../../../../modules/dictation/interface';
 
-const Container = styled.View`
+const Container = styled.ScrollView`
   padding-horizontal: ${props => props.theme.gridUnit * 6}px;
   padding-bottom: ${props => props.theme.gridUnit * 3}px;
   padding-top: ${props => props.theme.gridUnit}px;
   background-color: ${props => props.theme.colors.white};
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: space-around;
+  max-height: ${props => props.theme.gridUnit * 30}px;
 `;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+});
 
 interface Props {
   iscollapsed: boolean;
@@ -26,7 +33,7 @@ interface Props {
 export const AnswersArea = (props: Props) => {
   return (
     <Collapsible collapsed={props.iscollapsed}>
-      <Container>
+      <Container contentContainerStyle={styles.container}>
         {props.selectedChoiceInputChoices &&
           props.selectedChoiceInputChoices.map(selectedChoiceInputChoice => (
             <Answer
