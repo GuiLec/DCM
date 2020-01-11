1ae997afe57449e128dbe51cc9c823f907147425
GuiLec <glecallier@yahoo.com>
2020-01-11 09:53:42 +0100

    connect to aws dcm_api

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -2,7 +2,7 @@ import wretch from 'wretch';
 
 export const DCM_API = () =>
   wretch()
-    .url('http://localhost:3000')
+    .url('http://ec2-3-12-10-77.us-east-2.compute.amazonaws.com:3000')
     .catcher(400, error => {
       throw error;
     });
