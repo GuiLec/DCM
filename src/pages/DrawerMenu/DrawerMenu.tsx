61c9630feaebc636c19b42c1ec3224d63b4cf1b9
GuiLec <glecallier@yahoo.com>
2020-01-18 16:51:56 +0100

    Design user name

--- src/pages/DrawerMenu/DrawerMenu.tsx
+++ src/pages/DrawerMenu/DrawerMenu.tsx
@@ -19,6 +19,28 @@ const CrossIconContainer = styled.TouchableOpacity`
   margin: ${props => props.theme.gridUnit * 4}px;
 `;
 
+const ItemIcon = styled(Icon)`
+  color: ${props => props.theme.colors.white};
+  margin-right: ${props => props.theme.gridUnit * 3}px;
+  margin-left: ${props => props.theme.gridUnit * 5}px;
+`;
+
+const UserTitle = styled.Text`
+  font-size: ${props => props.theme.fontSizes.big};
+  color: ${props => props.theme.colors.white};
+  font-weight: bold;
+  flex: 1;
+  margin-right: ${props => props.theme.gridUnit}px;
+`;
+
+const UserSectionContainer = styled.View`
+  align-items: center;
+  padding: ${props => props.theme.gridUnit * 4}px;
+  margin-bottom: ${props => props.theme.gridUnit * 8}px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
 export const DrawerMenu = () => {
   const {menutItems} = useDrawerMenu();
   return (
@@ -26,6 +48,10 @@ export const DrawerMenu = () => {
       <CrossIconContainer onPress={closeDrawer}>
         <CrossIcon name="times" size={25} />
       </CrossIconContainer>
+      <UserSectionContainer>
+        <ItemIcon name="user" size={30} />
+        <UserTitle>Guilec</UserTitle>
+      </UserSectionContainer>
       {menutItems.map(item => (
         <DrawerMenuItem
           key={item.title}
