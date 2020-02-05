d27cd3436c351c865f02efdc74c947722c89ce24
GuiLec <glecallier@yahoo.com>
2020-02-05 18:37:19 +0100

    remove useless imports

--- src/navigation/actions.ts
+++ src/navigation/actions.ts
@@ -1,6 +1,5 @@
 import {DrawerActions} from 'react-navigation-drawer';
 import {dispatchNavigationAction} from './service';
-import {NavigationNavigateAction, NavigationActions} from 'react-navigation';
 
 const openDrawerAction = DrawerActions.openDrawer();
 const closeDrawerAction = DrawerActions.closeDrawer();
